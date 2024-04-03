c79c80d058d34d7e241ee15bb587d2ce1ee3db48
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-04-04 00:58:55 +0300

    reqister models at admin panel

--- airlines_api/admin.py
+++ airlines_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from airlines_api.models import Route, Passenger, AirplaneType, Airport, Airplane, Flight
 
-# Register your models here.
+admin.site.register(Route)
+admin.site.register(Passenger)
+admin.site.register(Airport)
+admin.site.register(Airplane)
+admin.site.register(Flight)
+admin.site.register(AirplaneType)
