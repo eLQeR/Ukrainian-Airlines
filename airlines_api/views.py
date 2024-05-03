2e20e714393bff3cc5ec18a193d5b3b627b2c393
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-05-03 10:59:22 +0300

    Make an option to get passed flight to admin. Create 'get_passed_flights' and 'get_departured_flight' for admin user with FlightAdminDetailSerializer. Add docstrings to get_transfer_ways functions

--- airlines_api/views.py
+++ airlines_api/views.py
@@ -3,10 +3,10 @@ import datetime
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Count, F
 from rest_framework import mixins, viewsets, generics, status
-from rest_framework.decorators import api_view, throttle_classes, action
+from rest_framework.decorators import api_view, throttle_classes, action, permission_classes
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
 
@@ -39,7 +39,8 @@ from airlines_api.serializers import (
     FlightDetailSerializer,
     TicketListSerializer,
     PassengerSerializer,
-    AirportDetailSerializer
+    AirportDetailSerializer,
+    FlightAdminDetailSerializer
 )
 
 
@@ -117,7 +118,7 @@ class AirplaneViewSet(
 class FlightViewSet(
     viewsets.ModelViewSet
 ):
-    queryset = Flight.objects.filter(is_completed=False)
+    queryset = Flight.objects.all()
     serializer_class = FlightSerializer
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAdminOrReadOnly,)
@@ -133,8 +134,25 @@ class FlightViewSet(
         passengers = [ticket.passenger for ticket in flight.tickets.all()]
         return Response(self.get_serializer(passengers, many=True).data, status=status.HTTP_200_OK)
 
+    @action(
+        detail=False,
+        methods=['GET'],
+        url_path="passed",
+        permission_classes=[IsAdminUser],
+        name='passed'
+    )
+    def get_passed_flights(self, request):
+        flights = self.get_queryset()
+        return Response(self.get_serializer(flights, many=True).data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['GET'], url_path="departured", permission_classes=[IsAdminUser], name='departured')
+    def get_departured_flight(self, request, pk=None):
+        flight = get_object_or_404(self.get_queryset(), pk=pk)
+
+        return Response(self.get_serializer(flight, many=False).data, status=status.HTTP_200_OK)
+
     def get_serializer_class(self):
-        if self.action == 'list':
+        if self.action in ('list', "get_passed_flights"):
             return FlightListSerializer
 
         elif self.action == 'retrieve':
@@ -143,6 +161,9 @@ class FlightViewSet(
         elif self.action == 'get_tickets':
             return TicketListSerializer
 
+        elif self.action == 'get_departured_flight':
+                return FlightAdminDetailSerializer
+
         elif self.action == 'get_passengers':
             return PassengerSerializer
 
@@ -150,6 +171,9 @@ class FlightViewSet(
 
     def get_queryset(self):
         queryset = self.queryset
+        if self.action not in ("get_departured_flight", "get_passed_flights"):
+            queryset = queryset.filter(is_completed=False)
+
         departure_date = self.request.query_params.get("departure_date", None)
         route_id = self.request.query_params.get("route", None)
         source_airport_id = self.request.query_params.get("source_airport", None)
@@ -164,7 +188,7 @@ class FlightViewSet(
         if departure_date:
             queryset = queryset.filter(departure_time__date=departure_date)
 
-        if self.action in ('list', "retrieve"):
+        if self.action in ('list', "retrieve", "get_passed_flights", "get_departured_flight"):
             queryset = queryset.select_related(
             ).annotate(tickets_avaliable=(F("airplane__rows") * F("airplane__seats_in_row")) - Count('tickets'))
         return queryset
@@ -179,11 +203,11 @@ class RouteViewSet(
     permission_classes = (IsAdminOrReadOnly,)
 
     def get_serializer_class(self):
-        if self.action == "create":
-            return RouteSerializer
+        if self.action == "list":
+            return RouteListSerializer
         if self.action == "retrieve":
             return RouteDetailSerializer
-        return RouteListSerializer
+        return RouteSerializer
 
     def get_queryset(self):
         queryset = self.queryset
@@ -192,7 +216,7 @@ class RouteViewSet(
         if source_id:
             queryset = queryset.filter(source_id=source_id)
         if destination_id:
-            queryset = queryset.filter(departure_time=destination_id)
+            queryset = queryset.filter(destination_id=destination_id)
         return queryset
 
 
@@ -266,6 +290,10 @@ class FivePerMinuteUserThrottle(UserRateThrottle):
 @api_view(["GET"])
 @throttle_classes([FivePerMinuteUserThrottle])
 def get_transfer_ways(request, *args, **kwargs):
+    """
+    That is  api view to get all available right or transfer
+    ways to certain airport and on certain date
+    """
     try:
         airport1, airport2, date = get_validated_data(request.query_params)
     except ObjectDoesNotExist:
@@ -277,6 +305,10 @@ def get_transfer_ways(request, *args, **kwargs):
 
 
 def get_ways_to_airport(airport1: Airport, airport2: Airport, date: datetime):
+
+    """"The function that tries to find right ways to certain airport,
+    otherwise call 'get_transfer_flights' function """
+
     right_ways = Flight.objects.filter(
         route__destination=airport2,
         route__source=airport1,
@@ -292,10 +324,12 @@ def get_ways_to_airport(airport1: Airport, airport2: Airport, date: datetime):
     if transfer_flights:
         return {"result": [FlightListSerializer(flights, many=True).data for flights in transfer_flights]}
 
-    return {"result": f"There are no any flight from {airport1.name} to {airport2.name}."}
+    return {"result": f"There are no any flight from {airport1.name} to {airport2.name} in {date.strftime('%Y-%m-%d')}"}
 
 
 def get_transfer_flights(airport1: Airport, airport2: Airport, date: str):
+    """That is am limited and optimized Dijkstra algorithm to find the shortest
+     paths from airport1 to airport2 by only one transfer airport"""
     transfer_flights = []
     flights_avaliable = Flight.objects.filter(
         departure_time__gt=date,
@@ -318,11 +352,11 @@ def get_transfer_flights(airport1: Airport, airport2: Airport, date: str):
             if (route_to_transfer.id,) in flights_avaliable.values_list("route"):
                 airports_as_transfer.append((route.source, (flights_avaliable.filter(route=route_to_transfer))))
 
-    for airport, transfer_flights in airports_as_transfer:
+    for airport, flights in airports_as_transfer:
         destination_flights = flights_avaliable.filter(route__source=airport, route__destination=airport2)
         if destination_flights:
             for destination_flight in destination_flights:
-                for transfer_flight in transfer_flights:
+                for transfer_flight in flights:
                     if transfer_flight.arrival_time < destination_flight.departure_time:
                         transfer_flights.append((
                             transfer_flight,
