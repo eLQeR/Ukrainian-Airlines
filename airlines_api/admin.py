196bec1b004efe74513885474658c0b0c470e22c
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-05-06 20:20:52 +0300

    add ticket to admin panel

--- airlines_api/admin.py
+++ airlines_api/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from airlines_api.models import Route, Passenger, AirplaneType, Airport, Airplane, Flight, Crew
+from airlines_api.models import (
+    Route,
+    Passenger,
+    AirplaneType,
+    Airport,
+    Airplane,
+    Flight,
+    Crew,
+    Ticket,
+)
 
 admin.site.register(Route)
 admin.site.register(Passenger)
@@ -8,3 +17,4 @@ admin.site.register(Airplane)
 admin.site.register(Flight)
 admin.site.register(AirplaneType)
 admin.site.register(Crew)
+admin.site.register(Ticket)
